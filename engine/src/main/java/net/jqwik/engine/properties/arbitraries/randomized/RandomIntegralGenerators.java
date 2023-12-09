6b4d8bb287861556207c7e46da4a60ab5b0978ab
AFungo <augustofungo@gmail.com>
2023-12-09 18:45:56 -0300

    Added and implement randoop to RandomIntegralGenerators

--- engine/src/main/java/net/jqwik/engine/properties/arbitraries/randomized/RandomIntegralGenerators.java
+++ engine/src/main/java/net/jqwik/engine/properties/arbitraries/randomized/RandomIntegralGenerators.java
@@ -1,11 +1,17 @@
 package net.jqwik.engine.properties.arbitraries.randomized;
 
 import java.math.*;
+import java.util.*;
 
 import net.jqwik.api.*;
 import net.jqwik.api.RandomDistribution.*;
 import net.jqwik.engine.properties.*;
 import net.jqwik.engine.properties.shrinking.*;
+
+import randoop.main.*;
+import randoop.main.randoopflags.*;
+
+import static java.lang.Math.*;
 
 public class RandomIntegralGenerators {
 
@@ -37,6 +43,24 @@ public class RandomIntegralGenerators {
 		};
 	}
 
+	public static <T> RandomGenerator<T> randoop(Class<T> clazz){
+		int seed = 0;//(int)(random()*1000);
+		RandoopObjectGenerator rog = new RandoopObjectGenerator(clazz);
+		rog.setSeed(seed);
+		rog.addFlag(new LiteralsFileFlag("/home/augusto/Documents/tesis/randoopObjectGenerator/literals/lits.txt"));
+		List<Object> obj = rog.generateObjects(20);
+		// for (Object o: obj) {
+		// 	System.out.println(o);
+		// }
+		return random -> {
+			if(obj.isEmpty()){
+				rog.setSeed(seed+1);
+				obj.addAll(rog.generateObjects(20));
+				// obj = rog.generateObjects(20);
+			}
+			return Shrinkable.unshrinkable((T) obj.remove(0));
+		};
+	}
 	private static void checkTargetInRange(Range<BigInteger> range, BigInteger value) {
 		if (!range.includes(value)) {
 			String message = String.format("Shrinking target <%s> is outside allowed range %s", value, range);
