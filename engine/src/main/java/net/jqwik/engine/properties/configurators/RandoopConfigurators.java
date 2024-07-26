3bc1dd412e452a972543377af86b6ad49a0c0055
AFungo <augustofungo@gmail.com>
2024-07-26 13:06:19 -0300

    Make a configurator for randoop for Classes annotation

--- engine/src/main/java/net/jqwik/engine/properties/configurators/RandoopConfigurators.java
+++ engine/src/main/java/net/jqwik/engine/properties/configurators/RandoopConfigurators.java
@@ -4,15 +4,25 @@ import net.jqwik.api.*;
 import net.jqwik.api.arbitraries.*;
 import net.jqwik.api.configurators.*;
 import net.jqwik.api.constraints.*;
-import net.jqwik.api.providers.*;
+import net.jqwik.api.randoop.*;
 
-public class RandoopIntegerRangeConfigurator extends ArbitraryConfiguratorBase {
+import java.util.*;
+
+public class RandoopConfigurators extends ArbitraryConfiguratorBase {
 
 	public Arbitrary<?> configure(Arbitrary<?> arbitrary, IntRange range) {
 		if (arbitrary instanceof RandoopArbitrary) {
 			RandoopArbitrary<?> randoopArbitrary = (RandoopArbitrary<?>) arbitrary;
 			return randoopArbitrary.setIntegersLiterals(range.min(), range.max());
 					   //greaterOrEqual(range.min()).lessOrEqual(range.max()!=Integer.MAX_VALUE ? range.max() : 10);
+		}
+		return arbitrary;
+	}
+
+	public Arbitrary<?> configure(Arbitrary<?> arbitrary, Classes classes) {
+		if (arbitrary instanceof RandoopArbitrary) {
+			RandoopArbitrary<?> randoopArbitrary = (RandoopArbitrary<?>) arbitrary;
+			return randoopArbitrary.setNecessaryClasses(new HashSet<>(Arrays.asList(classes.classes())));
 		}
 		return arbitrary;
 	}
