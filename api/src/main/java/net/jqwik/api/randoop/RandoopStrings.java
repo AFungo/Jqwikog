0ddd3d04c12e06e1ec849251f117e2829a3c3bf6
AFungo <augustofungo@gmail.com>
2024-10-21 11:55:42 -0300

    Make an annotation RandoopStrings

---
+++ api/src/main/java/net/jqwik/api/randoop/RandoopStrings.java
@@ -0,0 +1,19 @@
+package net.jqwik.api.randoop;
+
+import org.apiguardian.api.API;
+
+import java.lang.annotation.*;
+
+import static org.apiguardian.api.API.Status.MAINTAINED;
+
+/**
+ * Take all necessary classes for object generation
+ *
+ */
+@Target({ ElementType.ANNOTATION_TYPE, ElementType.PARAMETER, ElementType.TYPE_USE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@API(status = MAINTAINED, since = "1.0")
+public @interface RandoopStrings {
+	String[] strings();
+}
