f3307bbb1a6f5a51251137f29dcb83e46fc4da95
AFungo <augustofungo@gmail.com>
2024-11-05 11:54:46 -0300

    Change randoop Arbritary priority

--- engine/src/main/java/net/jqwik/engine/providers/RandoopArbitraryProvider.java
+++ engine/src/main/java/net/jqwik/engine/providers/RandoopArbitraryProvider.java
@@ -13,32 +13,23 @@ public class RandoopArbitraryProvider<T> implements ArbitraryProvider {
 
 	@Override
 	public boolean canProvideFor(TypeUsage targetType) {
-		return true;//NOTE: i put false because we only want use randoop when we don't have any option  always randoop can provide :)
+		return true;
 	}
 
 	@Override
 	public Set<Arbitrary<?>> provideFor(TypeUsage targetType, SubtypeProvider subtypeProvider) {
-		//NOTE: HERE we can parameterize randoop
 		List<TypeUsage> types = targetType.getTypeArguments();
 		List<Class<?>> clazzes = types.stream().map(TypeUsage::getRawType).collect(Collectors.toList());
-		// Set<List<Arbitrary<?>>> t = subtypeProvider.resolveAndCombine(types.toArray(new TypeUsage[0])).collect(CollectorsSupport.toLinkedHashSet());
-				   // .map(arbitraries -> {
-					//    Arbitrary<?> keyArbitrary = arbitraries.get(0);
-					//    Arbitrary<?> valueArbitrary = arbitraries.get(1);
-					//    return Arbitraries.maps(keyArbitrary, valueArbitrary);
-				   // })
-				   // .collect(CollectorsSupport.toLinkedHashSet());
-	// }
-		//FIXME: If we see another implementations there first call the api and the api calls the constructor. Maybe we need to do it like that
-		//NOTE: We can add the list of types there,
 		if(clazzes.isEmpty())
 			return Collections.singleton(new DefaultRandoopArbitrary<>(targetType.getRawType()));
 		else
 			return Collections.singleton(new DefaultRandoopArbitrary<>(targetType.getRawType(), clazzes));
 	}
 
+	private Integer priority = 100;
+
 	@Override
 	public int priority() {
-		return -1;
+		return priority;
 	}
 }
