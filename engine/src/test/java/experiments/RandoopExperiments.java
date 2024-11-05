e4d97465e3362da43c79512bbf656daf8744e833
AFungo <augustofungo@gmail.com>
2024-11-05 16:44:58 -0300

    Remove deprecated test

--- engine/src/test/java/experiments/RandoopExperiments.java
+++ engine/src/test/java/experiments/RandoopExperiments.java
@@ -16,30 +16,6 @@ import org.assertj.core.api.Assertions;
 public class RandoopExperiments {
 
 
-
-	public static boolean oddSize(Object o){
-		PilasTuple pilasTuple = (PilasTuple) o;
-		PilaSobreListasEnlazadas pila = pilasTuple.getPila();
-		int size = pila.length();
-		return size  % 2 != 0;
-	}
-
-	@Property(tries = 100)
-	void pilasTupleTest(
-		@ForAll @Deps(classes = {PilaSobreListasEnlazadas.class})
-		@AssumeMethod(className = RandoopExperiments.class, methodName = "oddSize")
-		@UseMethods(methods = {"setPilas", "setNumber", "applyPush"})
-		@IntRange(min = 110,max = 120) PilasTuple tuple)
-	{
-		System.out.println(tuple);
-	}
-
-
-
-
-
-
-
 	@Property(tries = 100)
 	public void sortedListTest(@ForAll @IntRange(min=20, max=30) SimpleList list){
 		Assume.that(list.size() > 2);
